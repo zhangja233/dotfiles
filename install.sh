c4859cbbc2a3dc3fc2e46151a23feef837be247d
Ju-an Zhang <zja148740@gmail.com>
2022-07-25 23:05:59 -0400

    the first workable installation

--- install.sh
+++ install.sh
@@ -1,3 +1,15 @@
 #! /bin/sh
-rm ~/.bashrc
-ln -s .bashrc ~/.bashrc
+
+path=`pwd`
+# sanity check
+dir=`echo $path | sed 's/.*\/\(.*\)/\1/'`
+if [ $dir != 'dotfiles' ]; then
+    echo "please run this script under dotfiles/"
+    exit
+fi
+
+if [ -f ~/.bashrc ]; then
+    rm ~/.bashrc
+fi    
+
+ln -s "$path/.bashrc" ~/.bashrc
